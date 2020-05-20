976d57f510ee70f01b9ca72015ef60de5dab73e4
Gijs Zwegers <gijszwegers@gmail.com>
2020-05-20 09:39:18 +0200

    added migration

---
+++ Api/Migrations/20200512231717_AddedDescriptionToRooms.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class AddedDescriptionToRooms : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Room",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Room");
+        }
+    }
+}
