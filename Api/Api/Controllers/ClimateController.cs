919a54975a8ca45600022d97bb18badb8252c774
KevinRomsom <kevin.romsom@gmail.com>
2020-06-02 22:16:20 +0200

    CRUD climate
    Crud van climate gemaakt. TODO: Gegevens van climate bewerken

---
+++ Api/Api/Controllers/ClimateController.cs
@@ -0,0 +1,48 @@
+﻿using Api.Api.EntityModels.Climate;
+using Api.Dal.Entities.Core;
+using Api.Logic.Interfaces;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClimateController : ControllerBase
+    {
+        private readonly IClimateService _climateLogic;
+
+        public ClimateController(IClimateService climateService)
+        {
+            _climateLogic = climateService;
+        }
+
+        [HttpPost("create")]
+        public async Task<JsonResult> Create(ClimateEntityModel climate)
+        {
+            return new JsonResult(await _climateLogic.Add(new Climate(0, climate.Name)));
+        }
+
+        [HttpPost("delete")]
+        public async Task<JsonResult> Delete(ClimateEntityModel climate)
+        {
+            return new JsonResult(await _climateLogic.Delete(climate.Id));
+        }
+
+        [HttpPost("update")]
+        public async Task<JsonResult> Update(ClimateEntityModel climate)
+        {
+            return new JsonResult(await _climateLogic.Update(new Climate(climate.Id, climate.Name)));
+        }
+
+        [HttpPost("get")]
+        public async Task<JsonResult> Get()
+        {
+            return new JsonResult(await _climateLogic.Get());
+        }
+    }
+}
---
+++ Api/Models/Climate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Models
+{
+    public class Climate
+    {
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Climate(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
